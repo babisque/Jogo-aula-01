913bb98cc445d91915d79bdbf0c8685229344329
babisque <rodrigobabisque1@gmail.com>
2022-04-12 14:37:22 -0300

    jogo criado

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Aula1___Jogo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sorteio = new Random().Next(1, 5);
+            Console.WriteLine("Tente adivinhar o número entre 1 e 5:");
+            int tentativa = 0;
+            while (tentativa != sorteio)
+            {
+                tentativa = int.Parse(Console.ReadLine());
+                if (tentativa == sorteio)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Você errou. Deseja tentar novamente? (S/n) ");
+                string tentar = Console.ReadLine();
+
+                if (tentar == "n")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Digite outra tentativa: ");
+            }
+
+            Console.WriteLine($"Você acertou!!! O número correto é {sorteio}");
+        }
+    }
+}
